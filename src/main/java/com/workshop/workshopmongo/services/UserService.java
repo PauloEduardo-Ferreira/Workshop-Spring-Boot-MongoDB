7fcbeb9f0cf5dd304e169d5980b1f367e7d1dd08
Paulo Eduardo <paulo.eduardoliveira@hotmail.com>
2025-11-26 11:20:36 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/workshop/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.workshop.workshopmongo.services;
+
+import com.workshop.workshopmongo.domain.User;
+import com.workshop.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
