e902b2b782ad6e3d33f2509241f528097352ffbf
Paulo Eduardo <paulo.eduardoliveira@hotmail.com>
2025-11-26 14:00:13 -0300

    Database instantiation operation

--- src/main/java/com/workshop/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/workshop/workshopmongo/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.workshop.workshopmongo.repository;
 
-import com.workshop.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.workshop.workshopmongo.domain.User;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-
 
 }
