81231903aadeadb448b2b1ab25df747e59441d97
Paulo Eduardo <paulo.eduardoliveira@hotmail.com>
2025-12-02 17:47:40 -0300

    Endpoint to return a user's posts

--- src/main/java/com/workshop/workshopmongo/resources/UserResource.java
+++ src/main/java/com/workshop/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.workshop.workshopmongo.resources;
 
+import com.workshop.workshopmongo.domain.Post;
 import com.workshop.workshopmongo.domain.User;
 import com.workshop.workshopmongo.dto.UserDTO;
 import com.workshop.workshopmongo.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
